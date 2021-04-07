c8b75edc162b21c7e0835367813c5b16ea5ced8b
alxayeed <alxayeed@gmail.com>
2021-04-07 11:03:54 +0600

    pillow and django-cloudinary library installed

--- django_cloudinary/settings.py
+++ django_cloudinary/settings.py
@@ -20,6 +20,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    # installed
+    'cloudinary_storage',
+
     'django.contrib.staticfiles',
     # my app
     'user',
