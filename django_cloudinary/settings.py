ad84e6681fe2078e81e20c8f6db951b9912dc1e5
alxayeed <alxayeed@gmail.com>
2021-04-07 11:07:43 +0600

    settings configured

--- django_cloudinary/settings.py
+++ django_cloudinary/settings.py
@@ -107,3 +107,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Cloudinary Configuration
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ['CLOUD_NAME'],
+    'API_KEY': os.environ['API_KEY'],
+    'API_SECRET': os.environ['API_SECRET']
+}
