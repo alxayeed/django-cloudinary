639c5bbe6dd7845e86cfbe224113b082b318b4c5
alxayeed <alxayeed@gmail.com>
2021-04-07 11:28:39 +0600

    Profile model created

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=256)
+    profile_image = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return self.name
